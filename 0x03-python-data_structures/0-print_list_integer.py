bec33ae8d365cc538289b48dabfb70fb3ae2cd9b
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-08-14 18:50:55 +0100

    fixing the code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
     """
     printing the elements in the list
     """
-    for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+    for num in my_list:
+        print("{}".format(num))
