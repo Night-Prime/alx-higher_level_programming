12cf0fa600ab9249138f3ae172c7668ac45ed3cb
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-08-17 02:42:53 +0100

    last-fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,8 @@
 
 
 def square_matrix_simple(matrix=[]):
-    return ([[(x**2) for x in row] for row in matrix])
+    new_matrix = []
+    for row in matrix:
+        new_row = [x**2 for x in row]
+        new_matrix.append(new_row)
+    return new_matrix
