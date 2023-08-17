92f1f342a612e68e7a4e080528fa565ffee203db
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-08-17 02:40:35 +0100

    fixing the function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,4 @@
 
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = [x**2 for x in row]
-        new_matrix.append(new_row)
-    return new_matrix
+    return ([[(x**2) for x in row] for row in matrix])
